0ce3329c5fc2e79d8189927f00e2d906397f6316
Hadi Rawal <hadirawal@gmail.com>
2023-11-09 12:54:21 +0100

    Register JS

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
+
 # Create your views here.
 
-
-
-class RegistrationView(View):
+class RegistrationView(View): #to reneder the page register.html (get html page)
     def get(self, request):
         return render(request, 'authentication/register.html')
     
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
+        return JsonResponse({'email_valid': True})
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
+        return JsonResponse({'username_valid': True})
