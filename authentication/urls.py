ccb78f70f790d367480611ef327b4564cbacc1cd
Hadi Rawal <hadirawal@gmail.com>
2023-11-10 11:10:03 +0100

    Password & Verification

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,13 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, LogoutView, VerificationView, LoginView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
      path('register', RegistrationView.as_view(), name="register"),
+     path('login', LoginView.as_view(), name="login"),
+     path('logout', LogoutView.as_view(), name="logout"),
      path('validate-username', csrf_exempt(UsernameValidationView.as_view()),name="validate-username"), #csrf :because django treat all post requests the same (Cross Site Request Forgery)
      path('validate-email', csrf_exempt(EmailValidationView.as_view()),name='validate-email'),
+     path('activate/<uidb64>/<token>',VerificationView.as_view(), name='activate'),
 ]
